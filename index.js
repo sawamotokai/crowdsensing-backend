05a7af751ffe24cc68b5a1bf765996b1d767130a
Kai Sawamoto <kaisawamoto@gmail.com>
2021-10-22 17:42:12 -0700

    majide

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.json());
-app.use("/api/v0", require("./api/v0/v0"));
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.json())
+app.use('/api/v0', require('./api/v0/v0'))
 
 // /getCompletionRateOfTasks
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   try {
   } catch (err) {
-    console.error(err);
-    return res.status(400).json(e);
+    console.error(err)
+    return res.status(400).json(e)
   } finally {
-    return res.send("backend serving...");
+    return res.send('backend serving...')
   }
-});
+})
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
+app.listen(port, () => console.log(`Server listening on port ${port}!!`))
